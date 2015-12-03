e50d8ec621e1ea902d253e2537034ea8748b9e9e
felixwa <felixwa@microsoft.com>
2015-12-03 22:12:10 +0800

    + Initial commit for Oxford Video SDK.

---
+++ Video/Windows/ClientLibrary/Contract/MotionDetectionResult.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.ProjectOxford.Video.Contract
+{
+    /// <summary>
+    /// Top-level object returned by the MotionDetection action.
+    /// </summary>
+    public class MotionDetectionResult : ResultBase
+    {
+        /// <summary>
+        /// Gets or sets an array of regions in which motions were captured.
+        /// </summary>
+        public MotionRegion[] Regions { get; set; }
+
+        /// <summary>
+        /// Gets or sets an array of fragments containing motion-detection events.
+        /// </summary>
+        public Fragment<MotionEvent>[] Fragments { get; set; }
+    }
+}
