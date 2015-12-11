729524705ebe977e7d0cb6df30880a30afe60eb9
Yung-Shin Lin <yslin@ntdev.microsoft.com>
2015-12-10 20:53:00 -0800

    Allow UWP apps to use emotion API

--- Emotion/Windows/ClientLibrary/Properties/AssemblyInfo.cs
+++ Emotion/Windows/ClientLibrary/Properties/AssemblyInfo.cs
@@ -52,9 +52,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("74d3d606-19c2-4341-b897-9d12ff751c38")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -65,5 +62,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
