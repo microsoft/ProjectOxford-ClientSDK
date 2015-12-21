778dcc122ff855a6418b60f285e6b9c74863398e
Xiao Liang (MSRA) <xiao.liang@microsoft.com>
2015-12-21 14:29:59 +0800

    Add Video API sample app

---
+++ Video/Windows/Sample-WPF/MotionDetectionPage.xaml.cs
@@ -0,0 +1,175 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Project Oxford: http://ProjectOxford.ai
+// 
+// Project Oxford SDK Github:
+// https://github.com/Microsoft/ProjectOxfordSDK-Windows
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+// -----------------------------------------------------------------------
+// KEY SAMPLE CODE STARTS HERE
+// Use the following namespace for VideoServiceClient
+// -----------------------------------------------------------------------
+using Microsoft.ProjectOxford.Video.Contract;
+// -----------------------------------------------------------------------
+// KEY SAMPLE CODE ENDS HERE
+// -----------------------------------------------------------------------
+
+namespace VideoAPI_WPF_Samples
+{
+    public partial class MotionDetectionPage : Page
+    {
+        private static readonly TimeSpan QueryWaitTime = TimeSpan.FromSeconds(20);
+        private const string LogIdentifier = "Motion Detection";
+
+        public MotionDetectionPage()
+        {
+            InitializeComponent();
+            DataContext = _dataContext;
+        }
+
+        // -----------------------------------------------------------------------
+        // KEY SAMPLE CODE STARTS HERE
+        // -----------------------------------------------------------------------
+        private async Task DetectMotion(string subscriptionKey, string originalFilePath)
+        {
+            _dataContext.IsWorking = true;
+            _dataContext.SourceUri = null;
+            _dataContext.ResultText = null;
+
+            Helpers.Log(LogIdentifier, "Start motion detection");
+            Microsoft.ProjectOxford.Video.VideoServiceClient client =
+                new Microsoft.ProjectOxford.Video.VideoServiceClient(subscriptionKey);
+
+            using (FileStream originalStream = new FileStream(originalFilePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] bytes = new byte[originalStream.Length];
+                await originalStream.ReadAsync(bytes, 0, (int)originalStream.Length);
+
+                // Creates a video operation of motion detection
+                Helpers.Log(LogIdentifier, "Start uploading video");
+                Operation operation = await client.CreateOperationAsync(bytes, OperationType.DetectMotion);
+                Helpers.Log(LogIdentifier, "Uploading video done");
+
+                // Starts querying service status
+                OperationResult result = await client.GetOperationResultAsync(operation);
+                while (result.Status != OperationStatus.Succeeded && result.Status != OperationStatus.Failed)
+                {
+                    Helpers.Log(LogIdentifier, "Server status: {0}, wait {1} seconds...", result.Status, QueryWaitTime.TotalSeconds);
+                    await Task.Delay(QueryWaitTime);
+                    result = await client.GetOperationResultAsync(operation);
+                }
+                Helpers.Log(LogIdentifier, "Finish processing with server status: " + result.Status);
+
+                // Processing finished, checks result
+                if (result.Status == OperationStatus.Succeeded)
+                {
+                    // Gets output JSON
+                    _dataContext.SourceUri = new Uri(originalFilePath);
+                    _dataContext.ResultText = Helpers.FormatJson<MotionDetectionResult>(result.ProcessingResult);
+                    _dataContext.FrameHighlights = GetHighlights(result.ProcessingResult).ToList();
+                }
+                else
+                {
+                    // Failed
+                    Helpers.Log(LogIdentifier, "Fail reason: " + result.Message);
+                }
+                _dataContext.IsWorking = false;
+            }
+        }
+        // -----------------------------------------------------------------------
+        // KEY SAMPLE CODE ENDS HERE
+        // -----------------------------------------------------------------------
+
+        /// <summary>
+        /// This method parses the JSON ouput, and converts to a sequence of time frames with highlight region.  A full video highlight is created if there is motion detected in the frame.
+        /// </summary>
+        /// <param name="json">JSON output of motion detection result.</param>
+        /// <returns>Sequence of time frames with highlight regions.</returns>
+        private static IEnumerable<FrameHighlight> GetHighlights(string json)
+        {
+            MotionDetectionResult motionDetectionResult = Helpers.FromJson<MotionDetectionResult>(json);
+
+            double timescale = motionDetectionResult.Timescale;
+            List<int> regionIds = motionDetectionResult.Regions.Select(x => x.Id).ToList();
+
+            Rect hasMotionRect = new Rect(new Point(0, 0), new Size(1, 1));  // Uses this full-frame rectangle to represent motion is detected in one frame
+            Rect noMotionRect = new Rect(new Point(0, 0), new Size(0, 0));   // Uses this empty rectangle to represent motion is not detected
+
+            foreach (Fragment<MotionEvent> fragment in motionDetectionResult.Fragments)
+            {
+                if (fragment.Events == null || fragment.Events.Length == 0)
+                {
+                    // If 'Events' is empty, there isn't any motion detected in this fragment
+                    Rect[] rects = new Rect[regionIds.Count];
+                    for (int i = 0; i < rects.Length; i++) rects[i] = noMotionRect;
+
+                    yield return new FrameHighlight() { Time = fragment.Start / timescale, HighlightRects = rects.ToArray() };
+                }
+                else
+                {
+                    long interval = fragment.Interval.GetValueOrDefault();
+
+                    for (int i = 0; i < fragment.Events.Length; i++)
+                    {
+                        double currentTime = (fragment.Start + interval*i)/timescale;
+
+                        MotionEvent[] evts = fragment.Events[i];
+
+                        Rect[] rects = regionIds.Select(id =>
+                        {
+                            MotionEvent evt = evts.FirstOrDefault(x => x.RegionId == id);
+                            if (evt == null) return noMotionRect;
+                            return hasMotionRect;
+                        }).ToArray();
+
+                        yield return new FrameHighlight() {Time = currentTime, HighlightRects = rects};
+                    }
+                }
+            }
+        }
+
+        private async void LoadImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            string originalFilePath = Helpers.PickFile();
+            if (originalFilePath == null) return;
+
+            await DetectMotion(Helpers.SubscriptionKey, originalFilePath);
+        }
+
+        private readonly VideoResultDataContext _dataContext = new VideoResultDataContext();
+    }
+}
---
+++ Video/Windows/Sample-WPF/VideoResultDataContext.cs
@@ -0,0 +1,101 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Project Oxford: http://ProjectOxford.ai
+// 
+// Project Oxford SDK Github:
+// https://github.com/Microsoft/ProjectOxfordSDK-Windows
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace VideoAPI_WPF_Samples
+{
+    class VideoResultDataContext : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Uri of original video
+        /// </summary>
+        public Uri SourceUri
+        {
+            get { return _sourceUri; }
+            set { _sourceUri = value; OnPropertyChanged(nameof(SourceUri)); }
+        }
+
+        /// <summary>
+        /// Uri of output video
+        /// </summary>
+        public Uri ResultUri
+        {
+            get { return _resultUri; }
+            set { _resultUri = value; OnPropertyChanged(nameof(ResultUri)); }
+        }
+
+        /// <summary>
+        /// Indicates whether service is working
+        /// </summary>
+        public bool IsWorking
+        {
+            get { return _isWorking; }
+            set { _isWorking = value; OnPropertyChanged(nameof(IsWorking)); }
+        }
+
+        /// <summary>
+        /// Stores result text from service
+        /// </summary>
+        public string ResultText
+        {
+            get { return _resultText; }
+            set { _resultText = value; OnPropertyChanged(nameof(ResultText)); }
+        }
+
+        /// <summary>
+        /// Uses this structure to display highlight rectangles at a specific time
+        /// </summary>
+        public IList<FrameHighlight> FrameHighlights
+        {
+            get { return _frameHighlights; }
+            set { _frameHighlights = value; OnPropertyChanged(nameof(FrameHighlights)); }
+        }
+
+
+        private Uri _sourceUri;
+        private Uri _resultUri;
+        private bool _isWorking;
+        private string _resultText;
+        private IList<FrameHighlight> _frameHighlights;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
